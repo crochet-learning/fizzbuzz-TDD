b34de73ed4c0efdce8f31d81c1828c2b5323d474
crochetcode <fred.davison@outlook.com>
2022-04-17 16:24:23 -0500

    end of chapter 1

--- index.test.js
+++ index.test.js
@@ -9,5 +9,15 @@ describe('fizzbuzz', () => {
     it('returns given number when not a multiple of 3 or 5', () => {
         expect(fizzbuzz(1)).toBe(1);
         expect(fizzbuzz(22)).toBe(22);
-    })
+    });
+
+    it('returns "Fizz" for multiples of 3', () => {
+        expect(fizzbuzz(3)).toBe('Fizz');
+        expect(fizzbuzz(9)).toBe('Fizz');
+    });
+
+    it('returns "Buzz" for multiples of 5', () => {
+        expect(fizzbuzz(5)).toBe('Buzz');
+        expect(fizzbuzz(500)).toBe('Buzz');
+    });
 });
